391d8a11b07eb0099574c04d0108c1e704d6a091
Dante-ML <emilianopr.ofi@gmail.com>
2024-10-18 07:10:33 -0600

    Seed data

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
+using API.Data;
 using API.Extensions;
 using API.Middlewares;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +17,21 @@ app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localho
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+        var context = services.GetRequiredService<DataContext>();
+        await context.Database.MigrateAsync();
+        await Seed.SeedUserAsync(context);
+} 
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurrred during migration / seeding.");
+}
+
 
 app.Run();
