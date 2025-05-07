548786af39af5d2301d5d6a062c291597ebbf6d0
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2025-05-07 09:28:07 -0600

    Add sort by to the API

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -3,6 +3,7 @@ using API.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace API.Data;
 public class UserRepository(DataContext context, IMapper mapper) : IUserRepository
@@ -33,6 +34,33 @@ public class UserRepository(DataContext context, IMapper mapper) : IUserReposito
         => await context.Users
                 .ProjectTo<MemberResponse>(mapper.ConfigurationProvider)
                 .ToListAsync();
+
+
+    public async Task<PagedList<MemberResponse>> GetMembersAsync(UserParams userParams)
+    {
+        var query = context.Users.AsQueryable();
+
+        query = query.Where(u => u.UserName != userParams.CurrentUsername);
+
+        if (userParams.Gender != null)
+        {
+            query = query.Where(u => u.Gender == userParams.Gender);
+        }
+
+        var minBDay = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+        var maxBDay = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+        query = query.Where(u => u.BirthDay >= minBDay && u.BirthDay <= maxBDay);
+
+        query = userParams.OrderBy.ToLower(CultureInfo.InvariantCulture) switch
+        {
+            "created" => query.OrderByDescending(x => x.Created),
+            _ => query.OrderByDescending(x => x.LastActive)
+        };
+
+        return await PagedList<MemberResponse>.CreateAsync(
+            query.ProjectTo<MemberResponse>(mapper.ConfigurationProvider), userParams.PageNumber, userParams.PageSize);
+    }            
 
     public async Task<bool> SaveAllAsync()
         => await context.SaveChangesAsync() > 0;
