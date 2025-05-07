2d0f0622635bcd285f94eb3e60afb68caf89fc0d
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2025-05-07 10:26:09 -0600

    Parcial05 - Add UserLike migration

--- API/DataEntities/AppUser.cs
+++ API/DataEntities/AppUser.cs
@@ -20,7 +20,7 @@ namespace API.Entities
         public required string Country { get; set; }
         public List<Photo> Photos { get; set; } = [];
 
-        //public int GetAge() => Birth.CalculateAge();
-
+        public List<UserLike> LikedByUsers { get; set; } = [];
+        public List<UserLike> LikedUsers { get; set; } = [];
     }
 }
