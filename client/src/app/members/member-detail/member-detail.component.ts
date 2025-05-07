7030d69e8db7cdcc3c794a52ac47a99cd009b872
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2025-05-07 12:32:47 -0600

    Parcial05 - Add message thread to SPA

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -6,14 +6,16 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TimeagoModule } from 'ngx-timeago';
 import { DatePipe } from '@angular/common';
+import { MemberMessagesComponent } from "../member-messages/member-messages.component";
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe],
+  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe, MemberMessagesComponent],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
+
 export class MemberDetailComponent implements OnInit{
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
@@ -26,6 +28,8 @@ export class MemberDetailComponent implements OnInit{
 
   loadMember() {
     const username = this.route.snapshot.paramMap.get("username");
+    console.log("Username:" + username);
+    
     if (!username) return;
     this.memberService.getMember(username).subscribe({
       next: (member) => {
