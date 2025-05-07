e9b9eed05c3b77eb627c6659fc67375b7cb53ae7
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2025-05-07 09:23:10 -0600

    Add filter buttons to SPA

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,25 +2,42 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { MemberCardComponent } from '../member-card/member-card.component';
-
+import { FormsModule } from '@angular/forms';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent],
+  imports: [MemberCardComponent, PaginationModule, FormsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
+
 export class MemberListComponent implements OnInit {
+  private accountService = inject(AccountService);
   membersService = inject(MembersService);
-  
+  userParams = new UserParams(this.accountService.currentUser());
+  genderList = [{value: "female", display: "Females"}, {value: "male", display: "Males"}]
+
   ngOnInit(): void {
-    if (this.membersService.members().length === 0) {
+    if (!this.membersService.paginatedResult()) {
       this.loadMembers();
     }
   }
 
+  resetFilters() {
+    this.userParams = new UserParams(this.accountService.currentUser());
+    this.loadMembers();
+  }
+
   loadMembers() {
-    this.membersService.getMembers();
+    this.membersService.getMembers(this.userParams);
+  }
+
+  pageChanged(event: any) {
+    if (this.userParams.pageNumber !== event.page) {
+      this.userParams.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
