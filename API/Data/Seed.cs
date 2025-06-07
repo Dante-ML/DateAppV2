b6af3522dfc4cc2db44d3719654913420b379923
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2025-06-07 08:37:51 -0600

    Parcial06 - Add Identity roles

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -8,7 +8,7 @@ namespace API.Data;
 [ExcludeFromCodeCoverage]
 public class Seed
 {
-    public static async Task SeedUserAsync(UserManager<AppUser> userManager)
+    public static async Task SeedUsersAsync(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync())
         {
@@ -20,10 +20,36 @@ public class Seed
         {
             return;
         }
+
+        var roles = new List<AppRole>
+        {
+            new() { Name = "Admin" },
+            new() { Name = "Member" },
+            new() { Name = "Moderator" }
+        };
+
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
+
+        var admin = new AppUser
+        {
+            UserName = "admin",
+            KnownAs = "Admin",
+            Gender = string.Empty,
+            City = string.Empty,
+            Country = string.Empty
+        };
+
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        await userManager.AddToRolesAsync(admin, ["Admin", "Moderator"]);
+
         foreach(var user in users!)
         {
             user.UserName = user.UserName!.ToLowerInvariant();
             await userManager.CreateAsync(user, "Pa$$w0rd");
+            await userManager.AddToRoleAsync(user, "Member");
         }
       }
     private static readonly JsonSerializerOptions ReadOptions = new()
