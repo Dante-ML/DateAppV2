76f9654ca63ed83b880048f7838b9af21d3a79c2
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2025-06-07 10:03:09 -0600

    Parcial06 - Display online presence

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
 import { ToastrService } from 'ngx-toastr';
@@ -11,6 +11,7 @@ export class PresenceService {
   hubsUrl = environment.hubsUrl;
   private hubConnection?: HubConnection;
   private toastr = inject(ToastrService);
+  onlineUsers = signal<string[]>([]);
 
   createHubConnection(user: User) {
     this.hubConnection = new HubConnectionBuilder()
@@ -21,11 +22,17 @@ export class PresenceService {
       .build();
     
     this.hubConnection.start().catch(error => console.log(error));
+
     this.hubConnection.on("UserIsOnline", username => {
       this.toastr.info(username + " is online!")
     });
+
     this.hubConnection.on("UserIsOffline", username => {
       this.toastr.warning(username + " went out to touch some grass!")
+    });
+
+    this.hubConnection.on("GetOnlineUsers", usernames => {
+      this.onlineUsers.set(usernames)
     });
   }
 
