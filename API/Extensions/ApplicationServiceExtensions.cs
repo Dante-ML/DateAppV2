5aa157637d3cc8bac8d475290d9b62e297d88b63
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2025-06-07 09:55:51 -0600

    Parcial06 - Add Presence Tracker with SignalR

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Interfaces;
 using API.Helpers;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -22,6 +23,7 @@ public static class ApplicationServiceExtensions
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         services.AddSignalR();
+        services.AddSingleton<PresenceTracker>();
         
         return services;
     }
