4f4dc39da70855b4bc2b5ebd53434ac9ef6af64f
Dante-ML <142956665+Dante-ML@users.noreply.github.com>
2025-06-07 08:26:11 -0600

    Parcial06 - Update the seed method

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -5,8 +5,9 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class DataContext(DbContextOptions options) : IdentityDbContext<AppUser, AppRole, int, IdentityUserClaim<int>, AppUserRole,
-        IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>(options)
+public class DataContext(DbContextOptions options) : IdentityDbContext<
+        AppUser, AppRole, int,
+        IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>(options)
 {
     public DbSet<UserLike> Likes { get; set; }
     public DbSet<Message> Messages { get; set; }
--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -8,32 +8,25 @@ namespace API.Data;
 [ExcludeFromCodeCoverage]
 public class Seed
 {
-      public static async Task SeedUserAsync(DataContext context)
-      {
-            if(await context.Users.AnyAsync())
-            {
-                return;
-            }
-
-            var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-            };
-            var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
-
-            if (users == null)
-            {
-                return;
-            }
-
-            foreach(var user in users!)
-            {
-                using var hmac = new HMACSHA512();
-
-                context.Users.Add(user);
-            }
-            
-            await context.SaveChangesAsync();
-      }  
+    public static async Task SeedUserAsync(UserManager<AppUser> userManager)
+    {
+        if (await userManager.Users.AnyAsync())
+        {
+            return;
+        }
+        var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData, ReadOptions);
+        if (users == null)
+        {
+            return;
+        }
+        foreach(var user in users!)
+        {
+            await userManager.CreateAsync(user, "Pa$$w0rd");
+        }
+      }
+    private static readonly JsonSerializerOptions ReadOptions = new()
+    {
+        AllowTrailingCommas = true
+    };
 }
